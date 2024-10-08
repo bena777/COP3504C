4fe9e8fd4c7144c3a306a0f4b1ef54260e0266e8
ben <109058028+bena777@users.noreply.github.com>
2024-10-08 11:49:01 -0400

    Update pakudex.py

--- projects/project2/pakudex.py
+++ projects/project2/pakudex.py
@@ -8,70 +8,73 @@ class Pakudex:
         self.going = True
         self.main_menu()
     def main_menu(self):
-        while self.going:
-            print("\nPakudex Main Menu")
-            print("-----------------")
-            print("1. List Pakuri")
-            print("2. Show Pakuri")
-            print("3. Add Pakuri")
-            print("4. Remove Pakuri")
-            print("5. Evolve Pakuri")
-            print("6. Sort Pakuri")
-            print("7. Exit")
-            print("")
-            action = input("What would you like to do? ")
-            try:
-                action = int(action)
-                if (action > 7) or (action < 1):
-                    action = action + " " # throws intentional error
-                if action == 1: # list pakuri
-                    names = self.get_species_list()
-                    if not names:
-                        print("No Pakuri currently in the Pakudex!")
-                    else:
-                        print("\nPakuri in Pakudex:")
-                        for paku, i in zip(names, range(1, len(self.pakudex) + 1)):
-                            print(f"{i}. {paku}")
-                elif action == 2: # show pakuri
-                    species = input("Enter the name of the species to display: ")
-                    if (self.get_species_list()) and (species in self.get_species_list()):
-                        print(f"\nSpecies: {species}")
-                        print(f"Level: {str(self.get_stats(species)[0])}")
-                        print(f"CP: {str(self.get_stats(species)[1])}")
-                        print(f"HP: {str(self.get_stats(species)[2])}")
-                    else:
-                        print("Error: No such Pakuri!")
-                elif action == 3: # add pakuri
-                    species = input("Species: ")
-                    if (self.get_species_list()) and (species in self.get_species_list()): # utilizes short-circuiting to check first if the list is not none to prevent error
-                        print("Error: Pakudex already contains this species!")
-                        continue
-                    global level_input
-                    level_input = input("Level: ")
-                    again = self.add_pakuri(species,level_input)
-                    while not again:
-                        if (self.get_species_list()) and (species in self.get_species_list()):
-                            species = input("Species: ")
+        try:
+            while self.going:
+                print("\nPakudex Main Menu")
+                print("-----------------")
+                print("1. List Pakuri")
+                print("2. Show Pakuri")
+                print("3. Add Pakuri")
+                print("4. Remove Pakuri")
+                print("5. Evolve Pakuri")
+                print("6. Sort Pakuri")
+                print("7. Exit")
+                print("")
+                action = input("What would you like to do? ")
+                try:
+                    action = int(action)
+                    if (action > 7) or (action < 1):
+                        action = action + " " # throws intentional error
+                    if action == 1: # list pakuri
+                        names = self.get_species_list()
+                        if not names:
+                            print("No Pakuri currently in the Pakudex!")
                         else:
-                            level_input = input("Level: ")
+                            print("\nPakuri in Pakudex:")
+                            for paku, i in zip(names, range(1, len(self.pakudex) + 1)):
+                                print(f"{i}. {paku}")
+                    elif action == 2: # show pakuri
+                        species = input("Enter the name of the species to display: ")
+                        if (self.get_species_list()) and (species in self.get_species_list()):
+                            print(f"\nSpecies: {species}")
+                            print(f"Level: {str(self.get_stats(species)[0])}")
+                            print(f"CP: {str(self.get_stats(species)[1])}")
+                            print(f"HP: {str(self.get_stats(species)[2])}")
+                        else:
+                            print("Error: No such Pakuri!")
+                    elif action == 3: # add pakuri
+                        species = input("Species: ")
+                        if (self.get_species_list()) and (species in self.get_species_list()): # utilizes short-circuiting to check first if the list is not none to prevent error
+                            print("Error: Pakudex already contains this species!")
+                            continue
+                        global level_input
+                        level_input = input("Level: ")
                         again = self.add_pakuri(species,level_input)
-                    print(f"Pakuri species {species} (level {str(level_input)}) added!")
-                elif action == 4: # remove pakuri
-                    remove = input("Enter the name of the Pakuri to remove: ")
-                    self.remove_pakuri(remove)
-                elif action == 5: # evolve pakuri
-                    evolve = input("Enter the name of the species to evolve: ")
-                    self.evolve_species(evolve)
-                elif action == 6: # sort pakuri
-                    if self.get_species_list():
-                        self.pakudex = sorted(self.pakudex,key=lambda p:p.get_species()) # help from https://stackoverflow.com/questions/403421/how-do-i-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
-                    print("Pakuri have been sorted!")
-                elif action == 7: # exit program
-                    print("Thanks for using Pakudex! Bye!")
-                    self.going = False
-                    break
-            except:
-                print("Unrecognized menu selection!")
+                        while not again:
+                            if (self.get_species_list()) and (species in self.get_species_list()):
+                                species = input("Species: ")
+                            else:
+                                level_input = input("Level: ")
+                            again = self.add_pakuri(species,level_input)
+                        print(f"Pakuri species {species} (level {str(level_input)}) added!")
+                    elif action == 4: # remove pakuri
+                        remove = input("Enter the name of the Pakuri to remove: ")
+                        self.remove_pakuri(remove)
+                    elif action == 5: # evolve pakuri
+                        evolve = input("Enter the name of the species to evolve: ")
+                        self.evolve_species(evolve)
+                    elif action == 6: # sort pakuri
+                        if self.get_species_list():
+                            self.sort_pakuri()
+                        print("Pakuri have been sorted!")
+                    elif action == 7: # exit program
+                        print("Thanks for using Pakudex! Bye!")
+                        self.going = False
+                        break
+                except:
+                    print("Unrecognized menu selection!")
+        except:
+            pass
 
 
     def get_species_list(self) -> list[str]:
@@ -79,6 +82,8 @@ class Pakudex:
             return None
         return [x.get_species() for x in self.pakudex]
 
+    def sort_pakuri(self):
+        self.pakudex = sorted(self.pakudex, key=lambda p: p.get_species())  # help from https://stackoverflow.com/questions/403421/how-do-i-sort-a-list-of-objects-based-on-an-attribute-of-the-objects
     def add_pakuri(self,species:str,level) -> bool:
         try:
             level = int(level)
@@ -121,7 +126,7 @@ class Pakudex:
             return None
         for i in self.pakudex:
             if i.get_species() == species:
-                return [i.level,i.combat_power,i.healthy_points] # returns in order [level,cp,hp]
+                return [i.level,i.cp,i.hp] # returns in order [level,cp,hp]
 
 
 def main():
